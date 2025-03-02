9bd72ac233f613f82ed5f0f16991c41473f95c06
Madman10K <stanislav.vasilev@metacraft-labs.com>
2025-03-02 17:32:45 +0000

    feat(deps): Add csources, nim and sqlite as submodules so that downloading dusing build is not required in the future

--- non-nix-build/install_nim_osx.sh
+++ non-nix-build/install_nim_osx.sh
@@ -2,34 +2,12 @@
 
 set -e
 
-WANTED_NIM_VERSION=v1.6.20
+cd ../libs/csources_v1 || exit
+make -j 8 || exit
+mv bin/nim ../nim/bin || exit
+cd ../nim/ || exit
 
-if command -v nim &> /dev/null; then
-  echo "Nim is already installed"
-  exit 0
-else
-  echo nim is missing! installing...
-fi
+bin/nim c koch.nim || exit
+./koch boot -d:release || exit
 
-: ${DEPS_DIR:=$PWD/deps}
-cd "$DEPS_DIR"
-
-# based on https://forum.nim-lang.org/t/10373#69081: from Araq:
-# but for v1
-
-rm -rf csources_v1/
-rm -rf nim/
-
-git clone https://github.com/nim-lang/csources_v1
-pushd csources_v1
-  make -j 8
-popd
-
-git clone https://github.com/nim-lang/nim
-mv csources_v1/bin/nim nim/bin
-
-pushd nim
-  git checkout $WANTED_NIM_VERSION
-  bin/nim c koch.nim
-  ./koch boot -d:release
-popd
+cd ../../non-nix-build || exit
