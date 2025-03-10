2f791cb340e81b2ae1271164b53fe7135782b0e1
Madman10K <stanislav.vasilev@metacraft-labs.com>
2025-03-10 13:29:41 +0000

    refactor(src/codetracer.nim): fix alexander's nitpicks about new lines and move startCore to backends

--- src/ct/launch/launch.nim
+++ src/ct/launch/launch.nim
@@ -9,30 +9,8 @@ import
   ../globals,
   cleanup,
   electron,
-  results
-
-# TODO: QA Note: move to separate file
-proc startCore*(traceArg: string, callerPid: int, test: bool) =
-  # start_core <trace-program-pattern> <caller-pid> [--test]
-  let recordCore = envLoadRecordCore()
-  var trace: Trace = nil
-  try:
-    let traceId = traceArg.parseInt
-    trace = trace_index.find(traceId, test=test)
-  except ValueError:
-    trace = trace_index.findByProgramPattern(traceArg, test=test)
-  except CatchableError as e:
-    errorMessage fmt"start core loading trace error: {e.msg}"
-    quit(1)
-
-  if trace.isNil:
-    echo "error: start core: trace not found for ", traceArg
-    quit(1)
-  # echo trace.repr
-  let process = startCoreProcess(traceId = trace.id, recordCore=recordCore, callerPid=callerPid, test=test)
-  let code = waitForExit(process)
-  discard code
-  stopCoreProcess(process, recordCore)
+  results,
+  backends
 
 proc runInitial*(conf: CodetracerConf) =
   # TODO should this be here?
