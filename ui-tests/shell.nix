719270cb2ca97a4ea22712d791d49df0873895b8
Alexander Ivanov <alehander42@gmail.com>
2025-03-13 14:00:13 +0200

    ci: add a simple shell.nix for now for ci usage for e2e tests

---
+++ ui-tests/shell.nix
@@ -0,0 +1,20 @@
+# copied and adapted from https://nix.dev/tutorials/first-steps/declarative-shell.html
+# however, using unstable like flake.nix
+let
+
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-unstable";
+
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+
+in
+
+
+pkgs.mkShellNoCC {
+
+  packages = with pkgs; [
+
+    xvfb-run
+
+  ];
+
+}
