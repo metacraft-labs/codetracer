24dc51b545514d86676f835cdc417181ec03b984
Petar Atanasov <p.atanasov21@abv.bg>
2025-03-21 17:16:37 +0200

    fix(dmg): Remove invalid option from create_dmg

--- non-nix-build/build_dmg.sh
+++ non-nix-build/build_dmg.sh
@@ -7,7 +7,6 @@ create-dmg \
   --window-size 600 400 \
   --icon-size 100 \
   --icon "CodeTracer.app" 150 190 \
-  --hide-extensions "CodeTracer.app" \
   --app-drop-link 450 185 \
   "CodeTracer.dmg" \
   "CodeTracer.app"
