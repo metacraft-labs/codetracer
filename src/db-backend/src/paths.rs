6e20a66de9dee045df1fc17840b2acf5afff519e
Madman10K <stanv.contact176@gmail.com>
2025-05-22 18:51:32 +0300

    bugfix(macos): patch for macOS

--- src/db-backend/src/paths.rs
+++ src/db-backend/src/paths.rs
@@ -13,16 +13,23 @@ impl Default for Paths
 {
     fn default() -> Self
     {
-        let tmpdir = PathBuf::from(env::var("TMPDIR").unwrap_or(
-            env::var("TEMPDIR").unwrap_or(
-                env::var("TMP").unwrap_or(
-                    env::var("TEMP").unwrap_or(
-                        "/tmp".to_string()
+        let tmpdir: PathBuf;
+        if cfg!(target_os = "macos")
+        {
+            tmpdir = PathBuf::from(env::var("HOME").unwrap()).join("Library/Caches/com.codetracer.CodeTracer/codetracer");
+        }
+        else
+        {
+            tmpdir = PathBuf::from(env::var("TMPDIR").unwrap_or(
+                env::var("TEMPDIR").unwrap_or(
+                    env::var("TMP").unwrap_or(
+                        env::var("TEMP").unwrap_or(
+                            "/tmp".to_string()
+                        )
                     )
                 )
-            )
-        )).join("codetracer/codetracer");
-        
+            )).join("codetracer/codetracer");
+        }
         Self {
             tmp_path: PathBuf::from(&tmpdir),
             client_socket_path: PathBuf::from(&tmpdir).join("ct_client_socket"),
