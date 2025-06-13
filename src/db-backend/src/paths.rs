78020cc1694020e04bad5390891a19516948a46b
Alexander Ivanov <alehander42@gmail.com>
2025-06-13 17:56:25 +0300

    cleanup: run `cargo fmt` and `cargo clippy` for db-backend

--- src/db-backend/src/paths.rs
+++ src/db-backend/src/paths.rs
@@ -1,42 +1,32 @@
-use std::path::PathBuf;
-use std::sync::{ Mutex, LazyLock };
 use std::env;
+use std::path::PathBuf;
+use std::sync::{LazyLock, Mutex};
 
-pub struct Paths
-{
+pub struct Paths {
     pub tmp_path: PathBuf,
     pub client_socket_path: PathBuf,
     pub socket_path: PathBuf,
 }
 
-impl Default for Paths
-{
-    fn default() -> Self
-    {
-        let tmpdir: PathBuf = if cfg!(target_os = "macos")
-        {
+impl Default for Paths {
+    fn default() -> Self {
+        let tmpdir: PathBuf = if cfg!(target_os = "macos") {
             PathBuf::from(env::var("HOME").unwrap_or("/".to_string())).join("Library/Caches/com.codetracer.CodeTracer/")
-        }
-        else
-        {
-            PathBuf::from(env::var("TMPDIR").unwrap_or(
-                env::var("TEMPDIR").unwrap_or(
-                    env::var("TMP").unwrap_or(
-                        env::var("TEMP").unwrap_or(
-                            "/tmp".to_string()
-                        )
-                    )
-                )
-            )).join("codetracer/")
+        } else {
+            PathBuf::from(
+                env::var("TMPDIR").unwrap_or(
+                    env::var("TEMPDIR")
+                        .unwrap_or(env::var("TMP").unwrap_or(env::var("TEMP").unwrap_or("/tmp".to_string()))),
+                ),
+            )
+            .join("codetracer/")
         };
         Self {
             tmp_path: PathBuf::from(&tmpdir),
             client_socket_path: PathBuf::from(&tmpdir).join("ct_client_socket"),
             socket_path: PathBuf::from(&tmpdir).join("ct_socket"),
         }
-    }   
+    }
 }
 
-pub static CODETRACER_PATHS: LazyLock<Mutex<Paths>> = LazyLock::new(|| {
-    Mutex::new(Paths::default())
-});
+pub static CODETRACER_PATHS: LazyLock<Mutex<Paths>> = LazyLock::new(|| Mutex::new(Paths::default()));
