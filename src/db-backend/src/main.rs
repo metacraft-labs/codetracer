5705e7d19a80819e348235c9e77bacbd8ef83d8e
Alexander Ivanov <alehander42@gmail.com>
2025-06-13 17:50:58 +0300

    fix: simplify single threaded-ness; small fixes; remove dap_session test
    * we were running everything important in one thread for now: so make it
    explicit, eventually we can separate dap from_reader in a separate thread
    and send to the dap dispatch which can remain with handler with not many code
    changes
    (or still separate dap loop/handling from handler.rs but not sure about that
    now)
    (this also stopped showing a thread error in the stdio test, but maybe the error was just
    because of stopping the process: it didn't fail the test)
    * remove/comment out/replace with log some eprint/print: problematic for
    `--stdio` mode
    * remove dap_session test: seems to reimplement too much of the dap server in
      itself

--- src/db-backend/src/main.rs
+++ src/db-backend/src/main.rs
@@ -17,7 +17,7 @@ use log::{error, info};
 use std::fs::File;
 use std::io::Write;
 use std::panic::PanicHookInfo;
-use std::thread;
+// use std::thread;
 use std::{error::Error, panic};
 
 mod calltrace;
@@ -95,11 +95,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let cli = Args::parse();
     info!("logging from db-backend");
 
-    eprintln!("pid {:?}", std::process::id());
-    let handle = if cli.stdio {
-        thread::spawn(move || {
-            let _ = db_backend::dap_server::run_stdio();
-        })
+    info!("pid {:?}", std::process::id());
+    // let handle = 
+    if cli.stdio {
+        // thread::spawn(move || {
+        let _ = db_backend::dap_server::run_stdio();
+        // })
     } else {
         let socket_path = if let Some(p) = cli.socket_path {
             p
@@ -107,13 +108,14 @@ fn main() -> Result<(), Box<dyn Error>> {
             let pid = std::process::id() as usize;
             db_backend::dap_server::socket_path_for(pid)
         };
-        thread::spawn(move || {
+        // thread::spawn(move || {
             let _ = db_backend::dap_server::run(&socket_path);
-        })
+        // })
     };
 
-    match handle.join() {
-        Ok(_) => Ok(()),
-        Err(_) => Err("dap server thread panicked".into()),
-    }
+    // match handle.join() {
+    //     Ok(_) => Ok(()),
+    //     Err(err) => Err(format!("dap server thread panicked {err:?}").into()),
+    // }
+    Ok(())
 }
