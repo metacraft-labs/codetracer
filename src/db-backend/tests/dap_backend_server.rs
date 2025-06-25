187c91208ddcb45a9ddfcc842f3036b854bc321a
pxor <68468634+pxor@users.noreply.github.com>
2025-06-25 13:10:09 +0300

    feat(dap): handle load-locals request

--- src/db-backend/tests/dap_backend_server.rs
+++ src/db-backend/tests/dap_backend_server.rs
@@ -44,6 +44,10 @@ fn test_backend_dap_server() {
         cwd: None,
         no_debug: None,
         restart: None,
+        name: None,
+        request: None,
+        typ: None,
+        session_id: None,
     };
     let launch = client.launch(launch_args);
     dap::write_message(&mut writer, &launch).unwrap();
