ce5e0a84963cb21a9396802ac1063518785a3806
Petar Atanasov <p.atanasov21@abv.bg>
2025-06-25 17:55:33 +0300

    fix(nix-lint): Remove explicit runtime_tracing pin from nix build

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -211,9 +211,6 @@
 
           cargoLock = {
             lockFile = ../../src/db-backend/Cargo.lock;
-            outputHashes = {
-              "runtime_tracing-0.11.0" = "sha256-zCfuJuWTiVqoxYkOW1kNtdSsX6mJXYp5AMOUtANHBOo=";
-            };
           };
 
           checkFlags = [
