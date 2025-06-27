bef0610a9d76dc5858fa505c9cb00584f5990555
Alexander Ivanov <alehander42@gmail.com>
2025-06-27 21:53:11 +0300

    feat: the viewmodel/mediator/dap abstractions for frontend: a bit more work
    still unfinished, but some work after response also from Zahary
    (telling about signal/slots and the two modes: websocket-like and
    same-process: here we for now assume we can have various separate
    custom: and starting with vscode one, but also do have local for
    same-process)

--- src/frontend/vscode.nim
+++ src/frontend/vscode.nim
@@ -1,7 +1,7 @@
 import std / [strformat, jsffi]
 import lib
 
-type 
+type
   VsCode* = ref object
     postMessage*: proc(js: JsObject): void
     # valid only in extension-level, not webview context probably
@@ -16,18 +16,9 @@ type
 var vscode*: VsCode
 
 type
-  DapRequest = ref object
-    command: cstring
-    value: JsObject
-
-type
-  CtEventKind* = enum
-    CtExample
-
   CtRawEvent* = ref object
     kind*: CtEventKind
     value*: JsObject
-
 
 type
   VsCodeTransport* = ref object
@@ -35,14 +26,15 @@ type
     messageHandlers*: array[CtEventKind, seq[proc(raw: JsObject): void]]
     onVsCodeMesssage*: proc(t: VsCodeTransport, event: JsObject)
 
-method emit*[T](t: Transport, event: T) =
-  t.vscode.postMessage(event.toJs)
+method emit*[T](t: Transport, eventKind: CtEventKind, value: T) =
+  t.vscode.postMessage(CtRawEvent(kind: eventKind, value: value.toJs))
 
 method subscribe*[T](t: Transport, eventKind: CtEventKind, callback: proc(value: T): Future[void]) {.base.} =
   # t.messageHandlers used in onVsCodeMessage : on window.addEventListener('message', (event) => t.onVsCodeMessage(event))
   # if not t.messageHandlers.hasKey(eventKind):
     # t.messageHandlers[eventKind] = @[]
   t.messageHandlers[eventKind].add(proc(raw: JsObject) = discard callback(cast[T](raw)))
+  t.vscode.postMessage(CtRawEvent())
 
 method onVsCodeMessage*(t: VsCodeTransport, event: CtRawEvent) =
   for handler in t.messageHandlers[event.kind]:
@@ -51,9 +43,66 @@ method onVsCodeMessage*(t: VsCodeTransport, event: CtRawEvent) =
     except:
       cerror fmt"handler for {event.kind} error: {getCurrentExceptionMsg()}"
 
-proc newVsCodeTransport(vscode: VsCode, vscodeWindow: JsObject): VsCodeTransport =
+proc newVsCodeTransport*(vscode: VsCode, vscodeWindow: JsObject): VsCodeTransport =
   result = VsCodeTransport(vscode: vscode)
   vscodeWindow.addEventListener(cstring"message", proc(event: CtRawEvent) =
     result.onVsCodeMessage(event))
 
+proc newVsCodeViewModelClient*(vscode: VsCode, vscodeWindow: JsObject): ViewModelClient =
+  let transport = newVsCodeTransport(vscode, vscodeWindow)
+  newViewModelClient(transport)
 
+
+
+proc newVsCodeViewModelServer*(vscode: VsCode): ViewModelServer {.exportc.}=
+   var dap = Dap(vscode: vscode)
+   let receive = proc(v: ViewModelServer, kind: CtEventKind, rawValue: JsObject, subscriber: Subscriber) =
+      if kind == CtSubscribe:
+        v.register(kind, subscriber)
+      elif true: # TODO: dap check
+        dap.sendCtRequest(kind, rawValue)
+   newViewModelServer(receive)
+
+type
+  Dap* = ref object
+    vscode: VsCode
+
+  DapRequest* = ref object
+    command*: cstring
+    value*: JsObject
+
+
+method toDapRequestCommand(dap: Dap, kind: CtEventKind): cstring =
+  let request = DapRequest(command: $kind, value: rawValue)
+  # TODO
+  # CtLoadLocals => "ct/load-locals"
+  # Step => "step" etc
+  cstring($kind)
+
+method sendCtRequest(dap: Dap, kind: CtEventKind, rawValue: JsObject) =
+  # TODO: await and handle response? emit for it?
+  discard dap.vscode.activeDebugSession.customRequest(dap.toDapRequestCommand(kind), rawValue)
+
+# TODO: vscode/dap specific wrappers if needed for receiving and emitting:
+# dap incoming responses/events calling `emit()` and 
+# panel-incoming events calling `receive`
+
+# for normal frontend: dap code also calling `emit()`
+# panel-incoming events would already call `receive`
+
+# receive might become the same! at least for javascript-based code
+# but with different dap instances: one wrapping vscode and one custom in our electron
+# other differences might be in coordinating updates to components/editor: those might be
+# overriden as well differently in vscode and in electron
+
+# vscode:
+# newVsCodeViewModelServer(vscode)
+
+# electron:
+# newViewModelServer(receive: .. receive)
+# 
+# newViewModelClient(newLocalTransport(server))
+
+    # self.api.emit(CtLoadLocals, "")
+
+# self.api.subscribe(CtLoadLocalsResponse, self.onLoadLocalsResponse)
