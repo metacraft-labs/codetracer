792446c8d45ea44660d984c78d4cda68b14d9f1b
Tzanko Matev <tsanko@metacraft-labs.com>
2025-07-16 16:07:41 +0300

    ci: Deploy docs via Github Pages
    Signed-off-by: Tzanko Matev <tsanko@metacraft-labs.com>

--- ci/deploy/docs.sh
+++ ci/deploy/docs.sh
@@ -10,10 +10,16 @@ pushd docs/book/
 # for now depending on global project devshell 
 mdbook build # build output is in the `book` directory
 
-# TODO: deploy book to codetracer.com ? ask Zahary?
-
-echo '###############################################################################'
-echo "TODO upload book"
-echo '###############################################################################'
+# If the gh-pages branch already exists, this will overwrite it
+# so that the history is not kept, which can be very expensive.
+git worktree add --orphan -B gh-pages gh-pages
+cp -a book/. gh-pages
+git config user.name "Deploy from CI"
+git config user.email ""
+cd gh-pages
+git add -A
+git commit -m 'deploy new book'
+git push origin +gh-pages
+cd ..
 
 popd
