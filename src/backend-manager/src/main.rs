982567e39c933ba56cae698d4a520c2be9eeffb0
Ro6afF <dimo_chanev@windowslive.com>
2025-07-28 17:05:23 +0300

    WIP: draft IPC communication for backend-manager

--- src/backend-manager/src/main.rs
+++ src/backend-manager/src/main.rs
@@ -4,8 +4,25 @@ mod errors;
 
 use std::error::Error;
 
+use tokio::{signal, sync::mpsc};
+
+use crate::backend_manager::BackendManager;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // TODO: maybe implement shutdown message?
+    let (shutdown_send, mut shutdown_recv) = mpsc::unbounded_channel::<()>();
+
+    let mgr = BackendManager::new().await?;
+    let mut mgr = mgr.lock().await;
+    mgr.spawn().await?;
+
+    tokio::select! {
+        _ = signal::ctrl_c() => {
+            println!("Ctrl+C detected. Shutting down...")
+        },
+        _ = shutdown_recv.recv() => {},
+    }
+
     Ok(())
 }
