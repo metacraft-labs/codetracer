9ba7df64104732a5cb143f0096d7e0c5400ee87d
Ro6afF <dimo_chanev@windowslive.com>
2025-08-06 10:50:05 +0300

    WIP: add simple logging for backend-manager

--- src/backend-manager/src/main.rs
+++ src/backend-manager/src/main.rs
@@ -1,21 +1,43 @@
+#[macro_use]
+extern crate log;
+
 mod backend_manager;
 mod dap_parser;
 mod errors;
 
 use std::error::Error;
 
+use clap::Parser;
 use tokio::{signal, sync::mpsc};
 
 use crate::backend_manager::BackendManager;
 
+#[derive(Parser, Debug)]
+#[command(version)]
+struct Cli {
+    /// Execute this command to start as ID 0
+    start: Option<String>,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    flexi_logger::init();
+
+    let cli = Cli::parse();
+
     // TODO: maybe implement shutdown message?
     let (_shutdown_send, mut shutdown_recv) = mpsc::unbounded_channel::<()>();
 
     let mgr = BackendManager::new().await?;
-    let mut mgr = mgr.lock().await;
-    mgr.start_replay().await?;
+
+    match cli.start {
+        Some(cmd) => {
+            let mut mgr = mgr.lock().await;
+            mgr.start_replay(&cmd).await?;
+        }
+
+        None => {}
+    }
 
     tokio::select! {
         _ = signal::ctrl_c() => {
