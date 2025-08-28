17bd561b10f7f68fd08f68e796926032d63993f1
Franz Fischbach <franz.fischbach@metacraft-labs.com>
2025-08-28 17:58:43 +0300

    fix(ui-tests-experimental): remove unused project, fix folder name

--- ui-tests-experimental/Helpers/CodetracerLauncher.cs
+++ ui-tests-experimental/Helpers/CodetracerLauncher.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Playwright;
 
-namespace UiTestsExperimental.Helpers
+namespace UtTestsExperimentalConsoleAppication.Helpers
 {
     public static class CodetracerLauncher
     {
@@ -82,4 +82,3 @@ namespace UiTestsExperimental.Helpers
         }
     }
 }
-
