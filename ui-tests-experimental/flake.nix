ad7af40e7d491ee1d913642725364cd716a29052
Alexander Ivanov <alehander42@gmail.com>
2025-08-29 16:04:00 +0300

    build: fix dependencies/dotnet run with Nikola on nixos

--- ui-tests-experimental/flake.nix
+++ ui-tests-experimental/flake.nix
@@ -27,12 +27,8 @@
         dotnet-full =
           with pkgs.dotnetCorePackages;
           combinePackages [
-            sdk_9_0
             sdk_8_0
-            runtime_9_0
             runtime_8_0
-            aspnetcore_9_0
-            aspnetcore_8_0
           ];
 
         deps = (
