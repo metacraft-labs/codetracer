78a27a91111fd91dd3d368b39b44b03eb6caae1e
Franz Fischbach <franz.fischbach@metacraft-labs.com>
2025-09-01 18:56:46 +0300

    feat(ui-tests): add example file structure for page objects

--- ui-tests-experimental/Program.cs
+++ ui-tests-experimental/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using UtTestsExperimentalConsoleAppication.Helpers;
@@ -10,18 +11,18 @@ class Program
 {
     public static async Task Main()
     {
-        System.Console.WriteLine("debug debug");
+
         if (!CodetracerLauncher.IsCtAvailable)
         {
             Console.WriteLine($"ct executable not found at {CodetracerLauncher.CtPath}. Build CodeTracer or set CODETRACER_E2E_CT_PATH.");
             return;
         }
 
-        var driver = await PlaywrightCodetracerLauncher.LaunchAsync("noir_space_ship");
+        var window = await PlaywrightCodetracerLauncher.LaunchAsync("noir_space_ship");
 
         try
         {
-            Console.WriteLine($"received driver: {driver}");
+            Console.WriteLine($"received driver: {window}");
             // await driver. ClickAsync("div");
             // var element = driver.FindElement(By.CssSelector(".menu-logo-img"));
             // isVisible = element.Displayed;
@@ -30,5 +31,20 @@ class Program
         {
             // Element not found; isVisible remains false.
         }
+
+        var driver = window.Contexts[0].Pages[0];
+
+        for (int i = 0; i < 5; i++)
+        {
+            await driver.Locator("#menu-root").ClickAsync();
+            Thread.Sleep(1000);
+        }
+
+        var kill = new ProcessStartInfo("just")
+        {
+            ArgumentList = { "stop" },
+        };
+        
+        var process2 = Process.Start(kill)!;
     }
 }
