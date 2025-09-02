02bfd945d7dc7ff1f336711c6dece9d5519fca79
Alexander Ivanov <alehander42@gmail.com>
2025-09-02 14:49:37 +0300

    build: build backend-manager for osx; remove a \ file for osx

---
+++ non-nix-build/build_backend_manager.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# depends on env `$ROOT_DIR` and `$DIST_DIR` and cargo/rust installed
+
+set -e
+
+echo "==========="
+echo "codetracer build: build backend-manager"
+echo "-----------"
+
+
+pushd "$ROOT_DIR/src/backend-manager"
+cargo build --release
+cp "$ROOT_DIR/src/db-backend/target/release/backend-manager" "$DIST_DIR/bin/backend-manager"
+popd
+
+echo "==========="
