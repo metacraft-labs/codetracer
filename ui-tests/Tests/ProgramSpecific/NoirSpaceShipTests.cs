1795fe506366d4d7063521ed2ef5bbbef3270091
Franz Fischbach <franz.fischbach@metacraft-labs.com>
2025-09-11 15:21:38 +0300

    fix(ui-tests): remove comments

--- ui-tests/Tests/ProgramSpecific/NoirSpaceShipTests.cs
+++ ui-tests/Tests/ProgramSpecific/NoirSpaceShipTests.cs
@@ -8,11 +8,7 @@ public static class NoirSpaceShipTests
     /// Ensure the Noir Space Ship example opens an editor tab titled "src/main.nr".
     /// </summary>
     public static async Task EditorLoadedMainNrFile(IPage page)
-    {
-        // TODO: write a retry find element function and place it in utils/RetryHelper
-        // TODO: use the helper from the previoud comment to create a wait function that confirms that at leas one editor tab is visible, place it it utils/WaitHelper
-        // TODO: Call the wait from the preious comment here
-        
+    {    
         var layout = new LayoutPage(page);
         var editors = await layout.EditorTabsAsync();
         if (!editors.Any(e => e.TabButtonText == "src/main.nr"))
@@ -24,26 +20,18 @@ public static class NoirSpaceShipTests
     public static async Task JumpToAllEvents(IPage page)
     {
         var layout = new LayoutPage(page);
-        // TODO: wait until a editor has been found
 
         var eventLogs = await layout.EventLogTabsAsync();
         foreach (var tab in eventLogs)
         {
             if (!await tab.IsVisibleAsync()) { continue; }
 
-            // check that at first all events are grayed out
             var events = await tab.EventElementsAsync();
             foreach (var e in events)
             {
                 // jump to an event by clicking on it
                 await e._root.ClickAsync();
             }
-
-            // TODO: check that the current event is not greyed out as well as all before it
-
-            // TODO: check that all events after the current one are still grayed out
-
-            // TODO: throw an FailedTestException if not true
         }
     }
 }
