bcf7e14e01117561b12fe0455a349dff28cd3904
Alexander Ivanov <alehander42@gmail.com>
2025-10-01 19:00:01 +0300

    fix: for now make it run with noop for some tasks; add more methods for Replay

--- src/db-backend/src/replay.rs
+++ src/db-backend/src/replay.rs
@@ -1,9 +1,20 @@
+use runtime_tracing::StepId;
 use std::error::Error;
 
 use crate::expr_loader::ExprLoader;
-use crate::task::Location;
+use crate::task::{Location, ProgramEvent};
+
+#[derive(Debug, Clone)]
+pub struct Events {
+    pub events: Vec<ProgramEvent>,
+    pub first_events: Vec<ProgramEvent>,
+    pub contents: String,
+}
 
 pub trait Replay: std::fmt::Debug {
     fn load_location(&mut self, expr_loader: &mut ExprLoader) -> Result<Location, Box<dyn Error>>;
     fn run_to_entry(&mut self) -> Result<(), Box<dyn Error>>;
+    fn load_events(&mut self) -> Result<Events, Box<dyn Error>>;
+    fn step_in(&mut self, forward: bool) -> Result<(), Box<dyn Error>>;
+    fn current_step_id(&self) -> StepId;
 }
