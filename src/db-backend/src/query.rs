78425365c4c8849cdbffe5f0d46fa3ca2c4792f4
Alexander Ivanov <alehander42@gmail.com>
2025-10-02 22:06:13 +0300

    feat: tried to abstract stepping/loading locals for db and rr
    loading locals still TODO in rr; overally not very tested in db

--- src/db-backend/src/query.rs
+++ src/db-backend/src/query.rs
@@ -1,8 +1,12 @@
 use serde::{Deserialize, Serialize};
 
+use crate::task::{Action, CtLoadLocalsArguments};
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(tag = "kind")]
 pub enum CtRRQuery {
     RunToEntry,
     LoadLocation,
+    Step { action: Action, forward: bool },
+    LoadLocals { arg: CtLoadLocalsArguments },
 }
