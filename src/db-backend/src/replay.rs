b195f5ef6e3a066b21d4c572f109deb7ee10ecfb
Alexander Ivanov <alehander42@gmail.com>
2025-10-14 23:06:43 +0300

    refactor: continue with flow_preloader refactoring: a bit more left

--- src/db-backend/src/replay.rs
+++ src/db-backend/src/replay.rs
@@ -1,6 +1,7 @@
-use runtime_tracing::StepId;
+use runtime_tracing::{StepId, ValueRecord};
 use std::error::Error;
 
+use crate::db::DbRecordEvent;
 use crate::expr_loader::ExprLoader;
 use crate::task::{Action, Location, ProgramEvent, CtLoadLocalsArguments, Variable};
 
@@ -17,6 +18,10 @@ pub trait Replay: std::fmt::Debug {
     fn load_events(&mut self) -> Result<Events, Box<dyn Error>>;
     fn step(&mut self, action: Action, forward: bool) -> Result<bool, Box<dyn Error>>;
     fn load_locals(&mut self, arg: CtLoadLocalsArguments) -> Result<Vec<Variable>, Box<dyn Error>>;
+    fn load_value(&mut self, expression: &str) -> Result<ValueRecord, Box<dyn Error>>;
+    // assuming currently in the right call for both trace kinds; and if rr: possibly near the return value
+    fn load_return_value(&mut self) -> Result<ValueRecord, Box<dyn Error>>;
+    fn load_step_events(&mut self, step_id: StepId, exact: bool) -> Vec<DbRecordEvent>;
     fn jump_to(&mut self, step_id: StepId) -> Result<bool, Box<dyn Error>>;
     fn current_step_id(&mut self) -> StepId;
 }
