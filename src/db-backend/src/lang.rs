8f764a1a24b6f0d9e7160723b1235b70b8ea4999
Alexander Ivanov <alehander42@gmail.com>
2025-10-24 23:27:49 +0300

    refactor: pass `lang` for some cases of loading values/locals

--- src/db-backend/src/lang.rs
+++ src/db-backend/src/lang.rs
@@ -1,5 +1,10 @@
+use std::path::Path;
+use std::ffi::OsStr;
+
 use num_derive::FromPrimitive;
 use serde_repr::*;
+
+use crate::task::TraceKind;
 
 #[derive(
     Debug,
@@ -35,4 +40,16 @@ pub enum Lang {
     Small,
     PythonDb,
     Unknown,
+}
+
+pub fn lang_from_context(path: &Path, trace_kind: TraceKind) -> Lang {
+    let extension = path.extension().unwrap_or(OsStr::new("")).to_str().unwrap_or("");
+    // for now important mostly for system langs/rr support
+    // but still good to add all supported langs: TODO
+    match extension {
+        "rs" => if trace_kind == TraceKind::DB { Lang::RustWasm } else { Lang::Rust },
+        "c" => Lang::C,
+        "cpp" => Lang::Cpp,
+        _ => Lang::Unknown,
+    }
 }
