a98142796bbe85379962d503eedb24f0166c025c
Madman10K <stanv.contact176@gmail.com>
2025-11-07 17:53:21 +0200

    hotfix: release 25.11.1

--- src/ct/version.nim
+++ src/ct/version.nim
@@ -6,7 +6,7 @@ import strutils
 
 const
   CodeTracerYear* = 25
-  CodeTracerMonth* = 10
+  CodeTracerMonth* = 11
   CodeTracerBuild* = 1
 
   CodeTracerVersionStr* = $CodeTracerYear & "." & ($CodeTracerMonth).align(2, '0') & "." & $CodeTracerBuild
