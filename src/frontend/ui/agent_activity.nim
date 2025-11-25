f1ecf2e2eea92dc799c2d51b5d6a39594e82ae2a
pxor <nedislav@gmail.com>
2025-11-25 17:59:09 +0200

    fix: Redesign of the terminal component and change input to textare for dynamic hight adjustment Uncomment the interactable part of the terminal. Still needs to be worked on TODO: The expand/collapse button on the terminal component

--- src/frontend/ui/agent_activity.nim
+++ src/frontend/ui/agent_activity.nim
@@ -1,14 +1,26 @@
 import ../utils, ../../common/ct_event, value, ui_imports, shell, command
+from dom import Node
+
+const HEIGHT_OFFSET = 2
+
+proc autoResizeTextarea(id: cstring) =
+  let el = document.getElementById(id)
+  if el.isNil: return
+
+  el.style.height = $(el.toJs.scrollHeight.to(int) + HEIGHT_OFFSET) & "px"
+  el.toJs.scrollTop = el.toJs.scrollHeight.to(int) + HEIGHT_OFFSET
 
 method render*(self: AgentActivityComponent): VNode =
+  let inputId = "agent-query-text"
   self.commandPalette = data.ui.commandPalette
   data.ui.commandPalette.agent = self
   if not self.kxi.isNil and not self.shell.initialized:
     self.kxi.afterRedraws.add(proc() =
+      self.inputField = cast[dom.Node](jq(fmt"#{inputId}"))
       self.shell.createShell() #TODO: Maybe pass in the lines and column sizes
       self.shell.initialized = true
-      self.shell.shell.write("Hello there, the terminal will wrap after 60 columns.\r\n")
-      self.shell.shell.write("Another line here.\r\n")
+      # self.shell.shell.write("Hello there, the terminal will wrap after 60 columns.\r\n")
+      # self.shell.shell.write("Another line here.\r\n")
     )
 
   result = buildHtml(
@@ -38,32 +50,43 @@ method render*(self: AgentActivityComponent): VNode =
             tdiv(class="command-palette-redo-button")
         tdiv(class="msg-content"):
           text "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, "
-          # TODO: For now hardcouded id - should be shellComponent-{custom-id}
-          tdiv(id="shellComponent-0", class="shell-container")
+          # TODO: For now hardcoded id - should be shellComponent-{custom-id}
+          tdiv(class="terminal-wrapper"):
+            tdiv(class="header-wrapper"):
+              tdiv(class="task-name"):
+                text "Running task..."
+              tdiv(class="msg-controls"):
+                tdiv(class="command-palette-copy-button", style=style(StyleAttr.marginRight, "6px".cstring))
+                tdiv(class="agent-model-img")
+            tdiv(id="shellComponent-0", class="shell-container")
         # TODO: Integrate it
     tdiv(class="agent-interaction"):
-      input(
+      textarea(
         `type` = "text",
-        id = "command-query-text",
-        name = "command-query",
+        id = inputId,
+        name = "agent-query",
         placeholder = "Ask anything",
         class = "mousetrap ct-input-cp-background agent-command-input",
         autocomplete="off", # https://stackoverflow.com/questions/254712/disable-spell-checking-on-html-textfields
         autocorrect="off",
         autocapitalize="off",
+        rows="1",
         spellcheck="false",
         onmousedown = proc =
           echo "#### SEARCH"
           discard,
-        oninput = proc(ev: Event, tg: VNode) =
-          let value = self.inputField.toJs.value.to(cstring),
-        onkeydown = proc(e: KeyboardEvent, v: VNode) =
-          echo "command ", e.keyCode
-          if e.keyCode == ENTER_KEY_CODE: # enter
-            #TODO: Add functionality
-            discard
-          # elif e.keyCode == ESC_KEY_CODE: # escape
-          #   self.commandPalette.resetCommandPalette()
+        onkeydown = proc (e: Event; n: VNode) =
+          let ke = cast[KeyboardEvent](e)
+          if ke.key == "Enter":
+            if ke.shiftKey:
+              return
+            else:
+              e.preventDefault()
+              # TODO SEND COMMAND
+        ,
+        oninput = proc (e: Event; n: VNode) =
+          self.inputValue = self.inputField.toJs.value.to(cstring)
+          autoResizeTextarea(inputId)
       )
       tdiv(class="agent-buttons-container"):
         tdiv(
