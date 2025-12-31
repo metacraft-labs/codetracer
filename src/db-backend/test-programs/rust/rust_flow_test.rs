7c07280e7811dd83e523709027f55d4c19765412
Alexander Ivanov <alehander42@gmail.com>
2025-12-31 11:17:42 +0200

    feat: try to detect `loop` and `while` too for rust flow loops; add loops to db-backend test program

--- src/db-backend/test-programs/rust/rust_flow_test.rs
+++ src/db-backend/test-programs/rust/rust_flow_test.rs
@@ -18,4 +18,32 @@ fn main() {
     let y = 32;
     let result = calculate_sum(x, y);
     println!("Result: {}", result);
+    with_loops(x);
+}
+
+fn with_loops(a: i32) {
+    let mut sum = 0;
+    for i in 0..a {
+        sum += i;
+    }
+    println!("sum with for {sum}");
+
+    sum = 0;
+    let mut i_2 = 0;
+    loop {
+        sum += i_2;
+        if i_2 >= a - 1 {
+            break;
+        }
+        i_2 += 1;
+    }
+    println!("sum with loop {sum}");
+
+    sum = 0;
+    let mut i_3 = 0;
+    while i_3 < a {
+        sum += i_3;
+        i_3 += 1;
+    }
+    println!("sum with while {sum}");
 }
