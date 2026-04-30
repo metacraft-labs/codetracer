e66e7046882902d6d43f92a0f0ece635a502d4a1
pxor <nedislav@gmail.com>
2026-04-30 13:06:36 +0300

    feat: Ui changes and fixes

--- nix/shells/default.nix
+++ nix/shells/default.nix
@@ -19,6 +19,15 @@
             config
             ;
         };
+        with-sui = import ./main.nix {
+          inherit
+            pkgs
+            self'
+            inputs'
+            config
+            ;
+          includeSui = true;
+        };
         ci = import ./ci.nix { inherit pkgs self'; };
       };
     };
