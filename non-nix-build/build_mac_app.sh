904ffbe4193bc71f23c054c76df9e77db4534bb4
pxor <nedislav@gmail.com>
2026-05-08 12:33:40 +0300

    fix: build script

--- non-nix-build/build_mac_app.sh
+++ non-nix-build/build_mac_app.sh
@@ -20,13 +20,29 @@ cp "$ROOT_DIR"/resources/Info.plist "$DIST_DIR"/..
 cd "$DIST_DIR"/..
 ln -s MacOS/node_modules ./
 
+ELECTRON_APP="$(realpath MacOS/node_modules)/electron/dist/Electron.app"
+if [ ! -d "$ELECTRON_APP/Contents" ]; then
+	echo ""
+	echo "ERROR: Electron.app bundle not found at: $ELECTRON_APP"
+	echo ""
+	echo "The Electron binary was not downloaded during 'yarn add electron'."
+	echo "Run the following to re-download it, then re-run build.sh:"
+	echo ""
+	echo "  cd $ROOT_DIR/node-packages"
+	echo "  npx yarn add electron --force"
+	echo "  # or if that still fails:"
+	echo "  node node_modules/electron/install.js"
+	echo ""
+	exit 1
+fi
+
 # Hack because basically every OS-level string is labeled as Electron instead CodeTracer. Can be resolved by using a bundler
-cp "$ROOT_DIR"/resources/Info.plist "$(realpath MacOS/node_modules)"/electron/dist/Electron.app/Contents/Info.plist
-cp "$DIST_DIR"/../Resources/CodeTracer.icns "$(realpath MacOS/node_modules)"/electron/dist/Electron.app/Contents/Resources/
+cp "$ROOT_DIR"/resources/Info.plist "$ELECTRON_APP/Contents/Info.plist"
+cp "$DIST_DIR"/../Resources/CodeTracer.icns "$ELECTRON_APP/Contents/Resources/"
 
 # macOS uses core utils from FreeBSD so the additional "" is needed to execute this sed call.
 #
 # This sed call is needed because even though we replaced the entire plist file, the CodeTracer icon will not be displayed correctly
 # in the auto-generated about menu, if the correct executable isn't listed here. If it was left as "bin/ct" a big disabled icon would
 # be overlayed on top of the app icon
-sed -i "" "s/\<string\>bin\/ct/\<string\>Electron/g" "$(realpath MacOS/node_modules)"/electron/dist/Electron.app/Contents/Info.plist
+sed -i "" "s/\<string\>bin\/ct/\<string\>Electron/g" "$ELECTRON_APP/Contents/Info.plist"
